171c1dbe4be4ff8c180755d84b6bad7230bc23a8
Russell Gordon <rgordon@rsgc.on.ca>
2017-11-06 07:59:43 -0500

    Initial Commit

---
+++ ShowingMyUnderstanding.playground/Pages/Checklist.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+/*:
+ 
+ [Previous](@previous)
+ 
+ # Checklist
+ 
+ On each page, did you demonstrate how to write human-readable code?
+ 
+ Across all pages in this playground, did you identify:
+ 
+ * where you are using comparison operators?
+ * where you are using arithmetic operators?
+ * where you are using variables?
+ * where you are using assignment statements?
+ 
+ */
---
+++ ShowingMyUnderstanding.playground/Pages/Sequence - Practice.xcplaygroundpage/Contents.swift
@@ -0,0 +1,45 @@
+/*:
+ [Previous](@previous) / [Next](@next)
+
+ # Sequence - Practice
+ 
+ When does sequence matter?
+
+ Here is an example of an image that is created and depends on statements being run in a certain order:
+
+ ![overlapping_shapes](overlapping-shapes.png "Overlapping Shapes Logo")
+
+ In this shape, there are lines and ellipses used.
+
+ [Color](http://russellgordon.ca/lcs/HSB_Color_Model_Summary_Swift.pdf) and alpha (transparency) matter.
+
+ Try reproducing this image for practice.
+ 
+ NOTE: Colors and image must match precisely.
+ 
+ ## Remember
+ 
+ Commit your work as you make progress on this page.
+ 
+ ## Note
+
+ The following two statements are required to make the playground run. Please do not remove.
+ */
+import Cocoa
+import PlaygroundSupport
+
+// Create canvas
+let canvas = Canvas(width: 300, height: 300)
+
+// No borders
+canvas.drawShapesWithBorders = false
+
+// Thick lines
+canvas.defaultLineWidth = 5
+
+// Add your code below... remember to use comments to indicate your intent
+/*:
+ ## Template code
+ The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
+ */
+PlaygroundPage.current.liveView = canvas.imageView
